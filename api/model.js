59865ef1fcf473b70488c4c5f8837dcf72470502
Elizabeth Doyle <123044077+elizabethmdoyle@users.noreply.github.com>
2024-03-25 13:23:09 -0400

    seeding

--- api/model.js
+++ api/model.js
@@ -1,27 +1,74 @@
-const db = require('../data/db-config.js');
+const db = require("../../data/db-config");
 
-function getSpecies() {
-  return db('species');
+async function getRecipeById(recipe_id) {
+  const recipeRows = await db("recipes as r")
+    .leftJoin("steps as s", "r.recipe_id", "s.recipe_id")
+    .leftJoin("step_ingredients as si", "si.step_id", "s.step_id")
+    .leftJoin("ingredients as i", "i.ingredient_id", "si.ingredient_id")
+    .select(
+      "r.recipe_id",
+      "r.recipe_name",
+      "s.step_id",
+      "s.step_number",
+      "s.step_text",
+      "i.ingredient_id",
+      "i.ingredient_name",
+      "si.quantity"
+    )
+    .orderBy("s.step_number")
+    .where("r.recipe_id", recipe_id);
+
+  const recipes = {
+    recipe_id: recipeRows[0].recipe_id,
+    recipe_name: recipeRows[0].recipe_name,
+    steps: recipeRows.reduce((acc, row) => {
+      if (!row.ingredient_id) {
+        return acc.concat({
+          step_id: row.step_id,
+          step_number: row.step_number,
+          step_text: row.step_text,
+          ingredients: [
+            {
+              ingredient_id: row.ingredient_id,
+              ingredient_name: row.ingredient_name,
+              quantity: row.quantity,
+            },
+          ],
+        });
+      }
+
+      const currentStep = acc.find((step) => step.step_id === row.step_id);
+      if (!currentStep) {
+        // If currentStep doesn't exist, create a new step
+        return acc.concat({
+          step_id: row.step_id,
+          step_number: row.step_number,
+          step_text: row.step_text,
+          ingredients: [
+            {
+              ingredient_id: row.ingredient_id,
+              ingredient_name: row.ingredient_name,
+              quantity: row.quantity,
+            },
+          ],
+        });
+      }
+
+      // If currentStep exists, push ingredients to the existing step
+      currentStep.ingredients.push({
+        ingredient_id: row.ingredient_id,
+        ingredient_name: row.ingredient_name,
+        quantity: row.quantity,
+      });
+
+      return acc;
+    }, []),
+  };
+
+  return recipes;
 }
 
-function getAnimals() { // INCLUDING SPECIES NAME
-  return db('animals as a')
-    .leftJoin('species as s', 's.species_id', 'a.species_id')
-    .select('a.animal_id', 'a.animal_name', 's.species_name');
-}
-
-async function createAnimal(animal) {
-  const [animal_id] = await db('animals').insert(animal);
-  return getAnimals().where({ animal_id }).first();
-}
-
-function deleteSpecies(species_id) {
-  return db('species').where({ species_id }).del();
-}
 
 module.exports = {
-  getSpecies,
-  getAnimals,
-  createAnimal,
-  deleteSpecies,
+  getRecipeById,
 };
--- api/router.js
+++ api/router.js
@@ -1,55 +1,19 @@
-const express = require('express');
-const Recipe = require('./model');
+const router = require("express").Router();
+const Recipe = require('');
 
-const router = express.Router();
-
-router.use('*', (req, res, next) => {
-res.json({api: 'up'})
-})
-
-router.use((err, req, res, next) => {// eslint-disable-line
-    res.status(500).json({
-        customMessage: 'something went wrong insude the recipes router',
-        message: err.message,
-        stack: err.stack,
-    })
-})
-
-
-router.get('/:recipie_id', (req, res, next) => {
-    Recipe.getRecipieByIsd(req.params.recipie_id)
-    .then(resource => {
+router.get("/:recipe_id", (req, res, next) => {
+  Recipe.getRecipeById(req.params.recipe_id)
+    .then((resource) => {
       res.status(200).json(resource);
     })
-    .catch(next); // our custom err handling middleware in server.js will trap this
+    .catch(next);
 });
 
-// router.get('/animals', (req, res, next) => {
-//   helpers.getAnimals() //INCLUDING SPECIES NAME
-//     .then(animals => {
-//       res.status(200).json(animals);
-//     })
-//     .catch(next);
-// });
-
-// router.post('/animals', (req, res, next) => { // it would be nice to have animal validation midd
-//   helpers.createAnimal(req.body)
-//     .then(animal => {
-//       res.status(201).json(animal);
-//     })
-//     .catch(next);
-// });
-
-// router.delete('/species/:species_id', (req, res, next) => { // it would be nice to have species_id validation midd
-//   helpers.deleteSpecies(req.params.species_id)
-//     .then(count => {
-//       if (count > 0) {
-//         res.status(204).end();
-//       } else {
-//         res.status(404).json({ message: 'Record not found' });
-//       }
-//     })
-//     .catch(next);
-// });
+router.use((err, req, res, next) => { //eslint-disable-line
+  res.status(500).json({
+    customMessage: "something went wrong inside the recipes router",
+    message: err.message,
+  });
+});
 
 module.exports = router;
